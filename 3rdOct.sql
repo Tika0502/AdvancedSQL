7f20768b1f2a5dd1506b03276d338f99ce067538
Tika0502 <129380259+Tika0502@users.noreply.github.com>
2025-10-02 15:29:04 +0200

    Update 3rdOct.sql

--- 3rdOct.sql
+++ 3rdOct.sql
@@ -12,3 +12,18 @@ ORDER BY
     color DESC,
     license_plate_number DESC;/');
 END;
+
+
+BEGIN adbk25f.upload_solution (102, 
+'SELECT
+    o_name,
+    address
+FROM
+    carmechanic.m_owner
+WHERE
+    address LIKE ''%Debrecen%''
+ORDER BY
+    o_name;/');
+END;
+
+
